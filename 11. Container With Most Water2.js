56ec43b855b416d684dd224fa6ba72445c218936
ahmedmahmoud <34657198+warhunter69@users.noreply.github.com>
2022-04-01 11:06:53 +0200

    Create 11. Container With Most Water2.js

---
+++ 11. Container With Most Water2.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let l = 0;
+    let r = height.length-1;
+    let result = 0;
+    let area = 0;
+    for(let i = 0 ; i < height.length ; i++){
+        result = Math.min(height[l],height[r])*(r-l);
+        if(result > area)
+            area = result;
+        
+        if(height[l]>=height[r]){
+            r--;
+        }else if(height[r]>height[l]){
+            l++;
+        }
+    }
+    return area;
+};
